2da345dc6aaea0483fa04679c1ad55e33b4a7b44
Jacob Polden <jacob.polden@gmail.com>
2017-04-11 11:51:51 +0100

    Added grunt task to copy dist

---
+++ JacobPoldenCodes.Umbraco/gruntfile.js
@@ -0,0 +1,46 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        copy: {
+            js: {
+                expand: true,
+                cwd: './node_modules',
+                dest: './Content/Scripts/',
+                flatten: true,
+                filter: 'isFile',
+                src: [
+                    './bootstrap/dist/js/bootstrap.js',
+                    './jquery/dist/jquery.js'
+                ]
+            },
+            css: {
+                expand: true,
+                cwd: './node_modules',
+                dest: './Content/Css/',
+                flatten: true,
+                filter: 'isFile',
+                src: [
+                    './bootstrap/dist/css/bootstrap.css',
+                    './bootstrap/dist/css/bootstrap-theme.css'
+                ]
+            }
+        },
+        watch: {
+            dist: {
+                files: [
+                    './node_modules/bootstrap/dist/css/bootstrap.css',
+                    './node_modules/bootstrap/dist/css/bootstrap-theme.css',
+                    './node_modules/bootstrap/dist/js/bootstrap.js',
+                    './node_modules/jquery/dist/jquery.js'
+                ],
+                tasks: ['default'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('default', ['copy']);
+};
